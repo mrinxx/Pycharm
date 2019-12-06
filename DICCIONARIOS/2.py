07c3873c75358aaf0abe1ebf3f1a84198ac30159
mrinxx <mrinxx5@gmail.com>
2019-12-06 12:59:44 +0100

    DESDE VSC

--- DICCIONARIOS/2.py
+++ DICCIONARIOS/2.py
@@ -1,14 +1,22 @@
 
-clasificacion={'PJ':0,'PG':0,'PP':0,'PAF':0,'PEC':0,'MAR':0}
-equipos={'CHB':clasificacion, 'CAW':clasificacion, 'CSU':clasificacion, 'COS':clasificacion, 'VIB':clasificacion,'CHE':clasificacion}
-print (equipos)
+puntuaciones={'PJ':0,'PG':0,'PP':0,'PAF':0,'PEC':0,'MAR':0}
+clasificacion={'CHB':puntuaciones, 'CAW':puntuaciones, 'CSU':puntuaciones, 'COS':puntuaciones, 'VIB':puntuaciones,'CHE':puntuaciones}
+#equipos={'CHB':,'CAW':[] 'CSU' 'COS':['Conil','suns','COS','Conil Suns'], 'VIB' 'CHE'}
 while True:
-    resultado=input("Introduce un resultado: ")
-    print(equipos)
-   #try:
-    #    resultado=int(resultado)
-     #   if resultado.lower == "fin":
-      #      print("SALIENDO DEL PROGRAMA")
-       #     break
-   # except ValueError:
-    #    print("Resultado no válido")
+    resultado=input("Introduce el equipo y sus puntos: ")
+    lista=str.split(resultado," ")
+    if lista[0] in clasificacion:
+        lista2=lista[1::]
+        l=dict(zip(puntuaciones,lista2))
+        clasificacion[lista[0]]=l
+        print(clasificacion[lista[0]])
+    #clavesclasif=clasificacion.keys()
+    #clavespunt=puntuaciones.keys()
+    #valores=puntuaciones.values()
+    #for p in clavespunt:
+     #   print("\t",p,sep=" ",end=" ")
+    #for i in clavesclasif:
+     #   print("\n",i,"\n",valores)
+    if resultado.lower() == "fin":
+        print("SALIENDO DEL PROGRAMA")
+        break
